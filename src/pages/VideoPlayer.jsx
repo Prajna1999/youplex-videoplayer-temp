7273929fcb9db8f5ef7dd6a9966ca96bc58a0d9b
Prajna1999 <gituprajna20@mail.com>
2022-08-14 14:10:33 +0530

    add video description

--- src/pages/VideoPlayer.jsx
+++ src/pages/VideoPlayer.jsx
@@ -24,15 +24,29 @@ function VideoPlayer() {
       <Navbar page="Go Back to Playlists" />
     </div>
 
-    <div className="flex ml-52 ">
+    <div className="flex ml-52 max-h-max mr-12 ">
         <div className="container mr-5 h-72 bg-blue-500">
             <h1>Placeholder for video player</h1>
         </div>
 
-        <div className="container w-4/5  h-screen">
+        <div className="container w-4/5">
                 <RichTextEditor />
         </div>
     </div>
+    <div className=" ml-52 flex justify-between mr-16 mt-4">
+        <div >
+            <h2 className="font-bold">How to Design a Card?</h2>
+            <p>This description to be fetched from fetch call.
+                Just a filler text for now.
+            </p>
+        </div>
+
+        <div className="bg-primary text-white h-max py-1 px-4 mr-1 rounded-lg">
+            <Link to='/schedule'>Schedule</Link>
+        </div>
+       
+    </div>
+
        
     
     </>
